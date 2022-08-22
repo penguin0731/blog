c15156ec0611f02196fcba6acc2c28ff5d52a372
penguin0731 <276087919@qq.com>
2022-08-22 16:55:33 +0800

    更新博客

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -54,6 +54,7 @@ module.exports = {
           { title: "介绍", path: "/JavaScript/介绍" },
           { title: "this对象", path: "/JavaScript/this对象" },
           { title: "执行上下文", path: "/JavaScript/执行上下文" },
+          { title: "作用域", path: "/JavaScript/作用域" },
           { title: "原型与继承", path: "/JavaScript/原型与继承" },
           { title: "ES6之Class", path: "/JavaScript/ES6之Class" },
           { title: "ES6之Symbol", path: "/JavaScript/ES6之Symbol" },
